726025e44a48ae408f659ad11f57cec41b47aeb4
Zachary Tarvid-Richey <zr.public@gmail.com>
2023-12-27 08:32:01 -0500

    Remove unneeded apk command

--- docker/entrypoints/webpack.sh
+++ docker/entrypoints/webpack.sh
@@ -4,17 +4,6 @@ set -e
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/*
 
-apk update && apk add --no-cache \
-  openssl \
-  tar \
-  build-base \
-  tzdata \
-  postgresql-dev \
-  postgresql-client \
-  nodejs-current \
-  yarn \
-  git \
-
 export NODE_OPTIONS=--openssl-legacy-provider
 
 yarn install
